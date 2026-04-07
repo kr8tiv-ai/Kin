ee3b6b39aca3256fe3fb83ee0b044896f5e4e850
Matt Haynes <lucidbloks@gmail.com>
2026-04-07 03:42:47 -0600

    gsd snapshot: pre-dispatch, uncommitted changes after 33m inactivity

--- bot/skills/builtins/index.ts
+++ bot/skills/builtins/index.ts
@@ -10,17 +10,20 @@ import { weatherSkill } from './weather.js';
 import { calculatorSkill } from './calculator.js';
 import { reminderSkill } from './reminder.js';
 import { webSearchSkill } from './web-search.js';
+import { browserSkill } from './browser.js';
 
 export const builtinSkills: KinSkill[] = [
   weatherSkill,
   calculatorSkill,
   reminderSkill,
   webSearchSkill,
+  browserSkill,
 ];
 
 export { weatherSkill } from './weather.js';
 export { calculatorSkill } from './calculator.js';
 export { reminderSkill } from './reminder.js';
 export { webSearchSkill } from './web-search.js';
+export { browserSkill } from './browser.js';
 
 export default builtinSkills;
